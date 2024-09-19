b97a5b9c92840d21df9c62c59f283434e707974e
Zach710366 <710366@pdsb.net>
2024-09-19 10:48:34 -0400

    Create Lesson3.py

---
+++ Lesson3.py
@@ -0,0 +1,8 @@
+bowl = 6
+cookie = 144
+print("Welcome to Sanjay's Koala Party!")
+print("It's possible Sanjay can distribute", 12)
+print("cookies for each bowl.")
+print("Sanjay has", bowl, "bowls for", cookie, "cookies.")
+# make sure to close your brackets next time ):
+print("The number of cookies per bowl is: %.0f" % (cookie/bowl))
