19e2c94f5f8fa7b8ef3a224a4e1d0fa338e241c5
Zach710366 <710366@pdsb.net>
2024-09-23 10:27:25 -0400

    Create Lesson4.py

---
+++ Lesson4.py
@@ -0,0 +1,4 @@
+num = int(input("Please input a whole number: "))
+N = int(input("Please input a whole number that isn't 0: "))
+num2 = num / N
+print("When", num, "is divided by", N, "the result is:", num2)
