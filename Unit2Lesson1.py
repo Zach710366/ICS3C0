88af756c5704ac8002d2133ecf3872d8d449f1aa
Zach710366 <710366@pdsb.net>
2024-10-03 12:04:26 -0400

    Create Unit2Lesson1.py

---
+++ Unit2Lesson1.py
@@ -0,0 +1,15 @@
+print("Make a choice from the following menu: ")
+print("A: apples")
+print("B: bananas")
+print("C: cherries")
+
+ch = input("Your choice: ")
+
+if (ch == "A"):
+    print("I prefer apples")
+elif (ch == "B"):
+    print("I prefer bananas")
+elif (ch == "C"):
+    print("I prefer cherries")
+else:
+    print("That wasn't an option")
